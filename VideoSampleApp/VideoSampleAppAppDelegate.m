691169d12117452c4a3bed680650914da2327b2f
Rangel Spasov <rangelspasov@Rangel-Spasovs-MacBook-Pro.local>
2011-07-26 13:50:24 -0700

    initial commit

--- VideoSampleApp/VideoSampleAppAppDelegate.m
+++ VideoSampleApp/VideoSampleAppAppDelegate.m
@@ -18,8 +18,8 @@
     //Init a G6PayAdManager object to gather video ads and display 
     
     videoAdManager = [[G6PayAdManager alloc] 
-                      initWithAppID:@"541"        
-                      prestitialSpaceID:@"1" delegate:self];
+                      initWithAppID:@"appId"        
+                      prestitialSpaceID:@"prestitialSpaceId" delegate:self];
     [videoAdManager playVideoAd];
     
     // Override point for customization after application launch.
